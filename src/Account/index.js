baf167917ce53af68053a41294475aa7f11aef0c
ZierkCodes <zierk.gage@gmail.com>
2021-08-06 15:34:10 -0400

    ensuring I have everything pushed

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 
-
 class Account extends Component {
+  constructor () {
+    super()
+    this.state = {
+      balance: 0,
+      textInput: React.createRef()
+    }
+  }
+  depositMoney = () => {
+    let balance = this.state.balance + Number(this.state.textInput.current.value)
+    this.setState({
+      balance,
+    })
+  }
+  withdrawMoney = () => {
+    let balance = this.state.balance - Number(this.state.textInput.current.value)
+    this.setState({
+      balance,
+    })
+  }
   render() {
     return (
-      <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+      <div className={this.state.balance === 0 ? "account zero" : "account"}>
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input ref={this.state.textInput} type="text" placeholder="enter an amount" />
+        <input onClick={this.depositMoney} type="button" value="Deposit" />
+        <input onClick={this.withdrawMoney}type="button" value="Withdraw" />
       </div>
     )
   }
 }
-
 export default Account;
